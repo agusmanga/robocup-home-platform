56f01603c28b7545a9a0f6bf3d17cc53b6ba453e
agusmanga <agustinmanganelli@gmail.com>
2026-09-10 02:47:43 +0000

    feat: configure gazebo

--- ros2_ws/src/robocup_bringup/launch/display.launch.py
+++ ros2_ws/src/robocup_bringup/launch/display.launch.py
@@ -1,14 +1,27 @@
+import os
+
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 import xacro
 
 def generate_launch_description():
     description_pkg = get_package_share_directory('robocup_description')
 
+    ros_gz_sim_pkg = get_package_share_directory('ros_gz_sim')
+
     xacro_file = description_pkg + '/urdf/robot.urdf.xacro'
 
     robot_description = xacro.process_file(xacro_file).toxml()
+
+    gazebo= IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(ros_gz_sim_pkg, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': '-r empty.sdf'}.items()
+    )
 
     rviz = Node(
         package='rviz2',
@@ -31,4 +44,15 @@ def generate_launch_description():
         name='joint_state_publisher',
         output='screen'
     )
-    return LaunchDescription([rviz, robot_state_publisher, joint_state_publisher])
+    robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        name='robot',
+        output='screen',
+        arguments=[
+            '-topic', '/robot_description',
+            '-name', 'robocup_robot',
+            '-z', '0.2'
+        ]
+    )
+    return LaunchDescription([rviz, gazebo, robot_state_publisher, joint_state_publisher, robot])
